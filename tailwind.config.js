1ab65cbe0d72c4d045e62bf9cb1a6ed7c07ddafb
MRP-rakib <rakib260.hossain@gmail.com>
2024-11-08 01:12:05 +0600

    cityproperties

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    fontFamily: {
+      'Nunito Sans': ['Nunito Sans', 'sans-serii',],
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'selector',
+}
+
